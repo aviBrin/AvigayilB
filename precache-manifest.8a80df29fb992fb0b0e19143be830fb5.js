3e9f62e87135897fcfb8ebace125ab353f3dcfb0
avigayil <avigah18@yahoo.com>
2019-12-30 20:25:15 +0200

    Updates

---
+++ precache-manifest.8a80df29fb992fb0b0e19143be830fb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74dbe89c59492952414d1ab95760b57d",
+    "url": "/AvigayilB/index.html"
+  },
+  {
+    "revision": "e1004bfd26bf92926609",
+    "url": "/AvigayilB/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8155803bf26e96448049",
+    "url": "/AvigayilB/static/js/2.30a2f15e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/AvigayilB/static/js/2.30a2f15e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1004bfd26bf92926609",
+    "url": "/AvigayilB/static/js/main.909e2e2f.chunk.js"
+  },
+  {
+    "revision": "93104f6b60c7c3d65d4f",
+    "url": "/AvigayilB/static/js/runtime-main.8378f42b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AvigayilB/static/media/logo.5d5d9eef.svg"
+  }
+]);
